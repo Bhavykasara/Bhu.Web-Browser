eb95906fe96f53b7767a01b285cd938646a180ef
Bhavykasara <78550596+Bhavykasara@users.noreply.github.com>
2021-06-04 17:02:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var a=document.getElementById("a");
+
+function search() {
+    if (document.getElementById("search").value==""){
+        console.log("null");
+     }
+     else {
+    document.getElementById("a").href="https://www.google.com/search?q="+document.getElementById("search").value;
+     }
+}
+
+window.addEventListener("keypress",op);
+
+function op(e){
+    keypressed=e.keyCode;
+    console.log(keypressed);
+    if (keypressed==13) {
+        if (document.getElementById("search").value==""){
+           console.log("null");
+        }
+        else {
+            window.open("https://www.google.com/search?q="+document.getElementById("search").value,"_self");
+        }
+    }
+}
+
+function c() {
+    $('#myModal').modal("show");
+}
